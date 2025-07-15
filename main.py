866834b77acdb05716254a4949c00ba1e7bbf449
Ander <sr.girou@gmail.com>
2025-07-15 19:43:06 +0200

    actualizacion

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ pg.init()
 pantalla = pg.display.set_mode( (1280, 720) )
 pg.display.set_caption( "Intro pygame ")
 
-
-
-
 game_over = True
 x = 0
 vx = 1
+y = 0
+vy = 1
+
 
 while game_over:
     for eventos in pg.event.get(): #capturar todos los eventos mientras se ejecuta el bucle
@@ -24,17 +24,20 @@ while game_over:
     pantalla.fill((60, 140, 100) ) #esto es la gama de colores de la pantalla
     #agregamos objeto a la pantalla
     x += vx
+    y += vy
+
     if x == 1280 or x == 0:
         vx = vx* -1
 
-
-
+    if y == 720 or y == 0:
+        vy = vy* -1
 
     #draw.rect(sourceface, color en (rgb) y posiciones(posicion en X y posicion en Y, y el tamaño del objeto, lo mismo, X e Y) ) 
 
-    pg.draw.rect(pantalla, (222, 58, 31),(630, 350, 10,10))
+    pg.draw.rect(pantalla, (222, 58, 31),(x, y, 10,10))
+    pg.draw.rect(pantalla, (222, 58, 31),(x+10, y+20, 10,10))
 
 
-    pg.display.flip() #
+    pg.display.flip() #funcion para cargar toda la configuracion que va dentro de la pantalla
 
 pg.quit()
